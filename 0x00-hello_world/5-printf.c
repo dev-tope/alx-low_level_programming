1a1a9fdeea6e544fb0a4d1c2fd6bd873653f0bb4
Your Name <topeoffical247@gmail.com>
2023-02-16 16:35:52 -0800

    done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Program that prints exactly the proper grammar
+ *
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-printf("with ptopper grammar, but the outcome is a piece of art,\n");
-return(0);
+	printf("with ptopper grammar, but the outcome is a piece of art,\n");
+	return(0);
 }
