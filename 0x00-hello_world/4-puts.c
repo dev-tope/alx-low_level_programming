7892e32f95b87011cbbd232fe0567d06beffdf65
Your Name <topeoffical247@gmail.com>
2023-02-16 16:42:19 -0800

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+
+/**
+ * main - puts to print out given sentence
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-puts("Programming is like building multilingual puzzle");
-return(0);
+	puts("Programming is like building multilingual puzzle\n");
+	return(0);
 }
