cbfe16062f79557ee0e5f86d27a1c4b408e9ce06
Your Name <topeoffical247@gmail.com>
2023-02-16 16:50:15 -0800

    done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Program that prints exactly the proper grammar
  *
@@ -7,5 +8,5 @@
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
