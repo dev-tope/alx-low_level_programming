02b14c3269bee3b71d75e32133d5ebe52c31c475
Your Name <topeoffical247@gmail.com>
2023-02-17 17:40:11 -0800

    Task 6 done

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,12 +9,13 @@
  */
 int main(void)
 {
-        int n;
-        
-        for (n = 0; n <= 9; n++)
-        {       
-        	putchar(n + '0');
-        }
-        putchar('\n');
-        return (0);
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+
+	return(0);
 }
