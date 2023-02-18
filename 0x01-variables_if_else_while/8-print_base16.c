636aecd96b2cdd19770366d4e8cebec584633590
Your Name <topeoffical247@gmail.com>
2023-02-18 13:26:31 -0800

    Mandatory tasks attempted

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description - Hexadecimal
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 16; i++)
+	{
+		putchar(i < 10 ? i + '0' : i + 'a' - 10);
+	}
+	putchar('\n');
+
+	return (0);
+}
