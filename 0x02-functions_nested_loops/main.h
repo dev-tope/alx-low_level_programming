9e8d71ca82b39ff07ffa32faede01bc2e60d6c2b
Your Name <topeoffical247@gmail.com>
2023-02-21 15:42:52 -0800

    Task 3 done

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-void int_islower(int c);
+void int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
