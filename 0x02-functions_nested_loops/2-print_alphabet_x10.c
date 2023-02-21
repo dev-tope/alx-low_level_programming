9e7f83a37ec27a66301ed0f4ac655326432f0574
Your Name <topeoffical247@gmail.com>
2023-02-21 15:19:19 -0800

    Task 2 attempted

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - Entry point
+* Description - Prints the alphabet 10x
+* Return: 0
+*/
+
+void print_alphabet_x10(void)
+{
+    int i;
+
+    for (i = 0; i <= 10; i++)
+    {
+        char c = 'a';
+
+        while (c <= 'z')
+        {
+            _putchar(c);
+            c++;
+        }
+
+        _putchar('\n');
+        i++;
+    }
+    return 0;
+}
