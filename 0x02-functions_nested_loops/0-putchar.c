17600d3b7886b7cb064c3734f45fdbbd829a1564
Your Name <topeoffical247@gmail.com>
2023-02-21 13:40:56 -0800

    Task 1 attempted

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description - A program that prints _putchar
+ *
+ * Return 0 Success
+ */
+int main(void)
+{
+	_putchar("_putchar");
+
+	return (0);
+}
