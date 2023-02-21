046b5ab62bb0812cbe8f1bec25c1da7278afc350
Your Name <topeoffical247@gmail.com>
2023-02-21 15:35:33 -0800

    Task 3 done

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ * Description - Check if c is a lowercase alphabet
+ * Return: 1 if true, 0 if false
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0)
+	}
+}
