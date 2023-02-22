45da212609443ad00f7edc8d29f28386361910ba
Your Name <topeoffical247@gmail.com>
2023-02-21 20:41:53 -0800

    All mandatory tasks attempted

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * Description - adds two integers together
+ * Return: int
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
