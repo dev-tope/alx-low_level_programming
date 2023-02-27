8eba039a76de217db33d24f1aa115010656399d7
dev-tope <topeofficial247@gmail.com>
2023-02-27 15:31:46 -0800

    Task 1 completed

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,8 +3,8 @@
 
 /**
  * reset_to_98 - Entry point
- * @*n: Pointer to memory
- * Description: tales a pointer to an int as parameter 
+ * @n: Pointer to memory
+ * Description: tales a pointer to an int as parameter
  * and updates the value it points to 98
  * Return void
  */
