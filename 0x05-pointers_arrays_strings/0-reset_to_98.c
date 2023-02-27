27df9b1fae72e004c8c6b27c02038f5b72e24d5e
dev-tope <topeofficial247@gmail.com>
2023-02-27 15:22:44 -0800

    Task 1 attempted

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry point
+ * @*n: Pointer to memory
+ * Description: tales a pointer to an int as parameter 
+ * and updates the value it points to 98
+ * Return void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
